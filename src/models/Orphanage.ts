edfccf0ad79cb989176853c95c35c0e46f55ae03
Thiago Trindade <thiago.trindadexd@gmail.com>
2020-10-17 15:17:12 -0300

    upload de images, listagem de imagens, views

--- src/models/Orphanage.ts
+++ src/models/Orphanage.ts
@@ -1,8 +1,16 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  JoinColumn,
+} from "typeorm";
 
-@Entity('orphanages')
+import Image from "./Image";
+
+@Entity("orphanages")
 export default class Orphanage {
-  @PrimaryGeneratedColumn('increment')
+  @PrimaryGeneratedColumn("increment")
   id: number;
 
   @Column()
@@ -25,4 +33,10 @@ export default class Orphanage {
 
   @Column()
   open_on_weekends: boolean;
+
+  @OneToMany(() => Image, (image) => image.orphanage, {
+    cascade: ["insert", "update"],
+  })
+  @JoinColumn({ name: "orphanage_id" })
+  images: Image[];
 }
